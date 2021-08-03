cb1a1d78a7bfb378ea854132640751c07e6fe6ea
raquelgothardt <71895033+raqgoth@users.noreply.github.com>
2021-08-03 10:07:50 -0400

    heroku postbuild

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const helmet = require('helmet')
 const connection = require('./db/connection')
 const path = require('path')
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT 
 const app = express()
 
 // Initialize Middleware
